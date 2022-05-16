13a70d178c97a64dee16a8efaf86d98aa2dfd3e9
wiryadev <abrarwiryawan@gmail.com>
2022-05-16 22:46:30 +0700

    reformat code

--- app/src/main/java/com/sennohananto/retrofit/ui/MainActivity.kt
+++ app/src/main/java/com/sennohananto/retrofit/ui/MainActivity.kt
@@ -9,9 +9,11 @@ import com.sennohananto.retrofit.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainActivityViewModel by viewModel()
     private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -33,7 +35,6 @@ class MainActivity : AppCompatActivity() {
                     progressDialog.setMessage("Loading...")
                     progressDialog.show()
                 }
-
                 Status.SUCCESS -> {
                     Toast.makeText(
                         this,
